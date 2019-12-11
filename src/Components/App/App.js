4452ba002d5563d8e10c5f92113a8176d0ec395b
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2019-12-11 11:26:14 -0700

    Remove console.logs

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -41,9 +41,7 @@ class App extends Component {
   }
 
   render() {
-    const { totalReservations, guestInfo } = this.state;
-    console.log(totalReservations)
-    console.log(guestInfo)
+    const { totalReservations } = this.state;
     return (
       <div className="App">
         <h1 className='app-title'>Turing Cafe Reservations</h1>
