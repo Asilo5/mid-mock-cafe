92f86c102fd63e1ba752ebe5859bd001940e2839
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2019-12-11 09:59:05 -0700

    Add basic layout to Reservation component and import it in ReservationsContainer

---
+++ src/Components/Reservation/Reservation.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Reservation = () => {
+  return (
+      <section>
+          <h2></h2>
+          <p></p>
+          <p></p>
+          <p>Number of guests: </p>
+          <button>Cancel</button>
+      </section>
+  )
+}
+
+export default Reservation;
--- src/Components/ReservationsContainer/ReservationsContainer.js
+++ src/Components/ReservationsContainer/ReservationsContainer.js
@@ -1,11 +1,11 @@
 import React from 'react';
-
+import Reservation from '../Reservation/Reservation';
 
 const ReservationsContainer = () => {
 
   return (
     <section className='reservations-container'>
-
+      <Reservation />
     </section>
   )
 }
