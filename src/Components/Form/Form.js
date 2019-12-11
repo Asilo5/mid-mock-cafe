81dd6e3018217256eaaf2eeac92b44b95573fb86
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2019-12-11 09:36:54 -0700

    Add basic layout of inputs and information needed on the form

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -11,7 +11,27 @@
      render() {
          return (
              <form>
-                 
+                 <input placeholder='Name' 
+                        type='text'
+                        name='name'
+                        value={this.state.name}
+                        onChange={this.handleChange} />
+                 <input placeholder='Date (mm/yy)' 
+                        type='text'
+                        name='date'
+                        value={this.state.date}
+                        onChange={this.handleChange} />
+                 <input placeholder='Time' 
+                        type='text'
+                        name='time'
+                        value={this.state.time}
+                        onChange={this.handleChange} />
+                 <input placeholder='Number of Guests' 
+                        type='text'
+                        name='guests'
+                        value={this.state.guests}
+                        onChange={this.handleChange} />
+                 <button>Make Reservation</button>
              </form>
          )
      }
