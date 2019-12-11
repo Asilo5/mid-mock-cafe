3b0173b73fe38f013177c3841470950e12364c56
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2019-12-11 10:14:43 -0700

    Drill down fetched reservations to Reservation component

--- src/Components/Reservation/Reservation.js
+++ src/Components/Reservation/Reservation.js
@@ -4,7 +4,7 @@ const Reservation = () => {
   return (
       <section>
           <h2></h2>
-          <p></p>
+          <p></p> 
           <p></p>
           <p>Number of guests: </p>
           <button>Cancel</button>
--- src/Components/ReservationsContainer/ReservationsContainer.js
+++ src/Components/ReservationsContainer/ReservationsContainer.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Reservation from '../Reservation/Reservation';
 
-const ReservationsContainer = () => {
-
+const ReservationsContainer = ({ totalReservations }) => {
+  const theReservations = totalReservations.map((reserv) => {
+    return <Reservation {...reserv} />
+  })
   return (
     <section className='reservations-container'>
-      <Reservation />
+      {theReservations}
     </section>
   )
 }
