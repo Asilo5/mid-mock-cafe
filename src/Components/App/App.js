60195c015a5b1b9a743dc848dbdecdde74c50db1
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2019-12-11 09:45:17 -0700

    Prop drill addGuestInfo to form

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -21,7 +21,7 @@ class App extends Component {
       <div className="App">
         <h1 className='app-title'>Turing Cafe Reservations</h1>
         <div className='resy-form'>
-          <Form />
+          <Form addGuestInfo={this.addGuestInfo} />
         </div>
         <div className='resy-container'>
           
